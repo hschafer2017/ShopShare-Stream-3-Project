cf8478ef7f047b901a148c197facf6bb73286fef
Haley Schafer <hschafer@umich.edu>
2018-05-28 14:11:51 +0000

    data structure and active users added and functional on chat page

--- shopshare.py
+++ shopshare.py
@@ -3,7 +3,23 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
-shopping_list = []
+shopping_list = {
+        'Nottingham': {
+            'user': "Haley",
+            'items':  [{
+                'user': "Haley",
+                'food': "Bread",
+            },
+            {
+                'user': 'Claire',
+                'food': 'TP',
+            },
+            {
+                'user': 'Meabh',
+                'food': 'pasta',
+            }]
+        }
+    }
 
 @app.route('/')
 def get_homepage():
@@ -16,20 +32,13 @@ def login():
     return redirect("/" + group + "/" + username) 
 # Pass two arguments through redirect without having to make an extra page 
 
-# @app.route('/<group>')
-# def get_group_page(group):
-#     return render_template('chat_page.html')
 
 @app.route('/<group>/<username>')
 def get_user_page(group, username):
-    return render_template('chat_page.html')
-    # return render_template("chat_page.html", group = group, username = username)
+    return render_template('chat_page.html', group = group, username = username, shop_items = shopping_list[group]['items'])
     
-# @app.route('/<group>/<username>/list')
-# def add_shopping_item(group,username):
-    
-#     return redirect(group,username)
-    
-    
+
     
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug = True)
+
+
